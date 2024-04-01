dffb808cc968dd7edf980c9d1c74c7371bc008ce
yuzhantao <yuzhantao@qq.com>
2024-04-02 00:57:55 +0800

    create

---
+++ cat2bug-jlog-demo/src/main/java/com/cat2bug/jlog/demo/Cat2bugJlogDemoApplication.java
@@ -0,0 +1,13 @@
+package com.cat2bug.jlog.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cat2bugJlogDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cat2bugJlogDemoApplication.class, args);
+    }
+
+}
---
+++ cat2bug-jlog-demo/src/test/java/com/cat2bug/jlog/demo/Cat2bugJlogDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cat2bug.jlog.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cat2bugJlogDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
